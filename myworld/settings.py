0358b530a94fbae872b9798c097ddd3697eb5f2a
Shubham Mehra <91693626+shubhammehra01@users.noreply.github.com>
2022-08-11 21:45:52 +0530

    Update settings.py

--- myworld/settings.py
+++ myworld/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR ,"static"),
+]
 STATIC_URL = 'static/'
 
 # Default primary key field type
