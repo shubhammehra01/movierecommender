0e122ecdad1ba17c0b014da001a309c917249fca
Shubham Mehra <91693626+shubhammehra01@users.noreply.github.com>
2022-08-11 23:10:42 +0530

    Update settings.py

--- myworld/settings.py
+++ myworld/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from settings import PROJECT_ROOT
+import PROJECT_ROOT
 from django.contrib.messages import constants as messages 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
