9a3a499909ff81d7997ea09988f801f4f4013150
Shubham Mehra <91693626+shubhammehra01@users.noreply.github.com>
2022-08-19 01:13:53 +0530

    Update urls.py

--- myworld/urls.py
+++ myworld/urls.py
@@ -8,5 +8,8 @@ from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('myapp.urls')), 
+    path('',include('myapp.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
